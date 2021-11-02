bc93d40de8af74d2a007e278260f79afec8a451a
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-11-02 19:14:19 +0000

    Add solution to ETL exercise

--- exercises/practice/etl/etl.py
+++ exercises/practice/etl/etl.py
@@ -1,2 +1,20 @@
-def transform(legacy_data):
-    pass
+from typing import Dict, List
+
+
+# The first step in writing this function was having a look at etl_test.py to find out
+# what format the legacy data was in, and what the expected output was supposed to be.
+# I decided to add type hints to the function signature to serve as a reminder of what
+# how exactly the data was stored and how it is transformed.
+def transform(legacy_data: Dict[int, List[str]]) -> Dict[str, int]:
+    """
+    Take Scrabble scores stored in a legacy format and return it in the new format
+    """
+    # Initialise what we're going to return
+    new_score_map = {}
+
+    # Iterating over dict.items() returns a (key, value) tuple, which we can unpack
+    for score, letter_list in legacy_data.items():
+        for letter in letter_list:
+            new_score_map[letter.lower()] = score
+
+    return new_score_map
